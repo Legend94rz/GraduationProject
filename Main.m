38a299b78eb5800091c1dfebb85c541d82fdac4a
legend <Legend94rz@gmail.com>
2017-06-07 00:15:57 +0800

    modify Main.m to a function

--- Main.m
+++ Main.m
@@ -1,84 +1,97 @@
-keySet = {'svmguide1','a7a','w7a','cbcl','ijcnn1'};
-valSet = {{},...
-		  {},...
-		  {},...
-		  {},...
-		  {}};
-ReCals = containers.Map(keySet,valSet);
+function Main()
+	keySet = {'svmguide1','a7a','w7a','cbcl','ijcnn1'};
+	valSet = {{},...
+			  {},...
+			  {},...
+			  {},...
+			  {}};
+	ReCals = containers.Map(keySet,valSet);
 
-dataNames=char({'svmguide1','a7a','w7a','cbcl','ijcnn1'});
-funcNames=char({'PA','CW','SCW','LOL','BSGD','Pegasos','Pegasos(single)'});
-[dataLength,~] = size(dataNames);
-[funcLength,~] = size(funcNames);
-ALL=[];
-for i=1:dataLength
-	dataset=strtrim(dataNames(i,:));
-	A = zeros(funcLength,5);
-	for j=1:funcLength
-		funcName = strtrim(funcNames(j,:));
-		if(~strcmp(funcName,'Pegasos(single)'))
-			[valid,~] = readOpt(dataset,funcName);
-			if(valid==0 || (ReCals.isKey(dataset) && sum( ismember(ReCals(dataset),funcName)))>0 )
-				fun = str2func(strtrim(funcNames(j,:)));
-				fun(dataset);
+	dataNames=char({'svmguide1','a7a','w7a','cbcl','ijcnn1'});
+	funcNames=char({'PA','CW','SCW','LOL','BSGD','Pegasos','Pegasos(single)'});
+	[dataLength,~] = size(dataNames);
+	[funcLength,~] = size(funcNames);
+	ALL=[];
+	for i=1:dataLength
+		dataset=strtrim(dataNames(i,:));
+		A = zeros(funcLength,5);
+		for j=1:funcLength
+			funcName = strtrim(funcNames(j,:));
+			if(~strcmp(funcName,'Pegasos(single)'))
+				[valid,~] = readOpt(dataset,funcName);
+				if(valid==0 || (ReCals.isKey(dataset) && sum( ismember(ReCals(dataset),funcName)))>0 )
+					fun = str2func(strtrim(funcNames(j,:)));
+					fun(dataset);
+				end
+			else
+				funcName='Pegasos_single';		%为了避免图例显示错误
 			end
-		else
-			funcName='Pegasos_single';		%为了避免图例显示错误
+			[valid,optCM] = readOpt(dataset,funcName);
+			A(j,:) = optCM;
 		end
-		[valid,optCM] = readOpt(dataset,funcName);
-		A(j,:) = optCM;
+		ALL = cat(3,ALL,A);
 	end
-	ALL = cat(3,ALL,A);
+	y=1:5;
+	vcount = ceil(sqrt(dataLength));
+	hcount = ceil(dataLength/vcount);
+
+	figure;
+	for i=1:dataLength
+		dataset=strtrim(dataNames(i,:));
+		subplot(vcount,hcount,i);
+		A = ALL(:,:,i);
+		b=bar(y,A','grouped');title(dataset);
+		ax = b.Parent;
+		ax.XTickLabel = {'tp','fp','fn','tn','P'};
+		ax.YTick=0:0.2:1;
+	end
+	legend(funcNames);
+	save('ALL.mat','ALL');
+
+	figure;
+	ratio=zeros(funcLength,1);
+	for i=1:dataLength
+		dataset=strtrim(dataNames(i,:));
+		subplot(vcount,hcount,i);
+		A = ALL(:,:,i);
+		SortAndBarh(A(:,1),dataset,funcNames,0:0.2:1);
+		ratio = ratio + A(:,1);
+	end
+	ratio = ratio/dataLength;
+	figure;
+	SortAndBarh(ratio,'平均真正例率',funcNames,0:0.2:1);
+	
+	figure;
+	ratio = zeros(funcLength,1);
+	for i=1:dataLength
+		dataset=strtrim(dataNames(i,:));
+		subplot(vcount,hcount,i);
+		A = ALL(:,:,i);
+		SortAndBarh(A(:,4),dataset,funcNames,0:0.2:1);
+		ratio = ratio+A(:,4);
+	end
+	ratio = ratio/dataLength;
+	figure;
+	SortAndBarh(ratio,'平均真反例率',funcNames,0:0.2:1);
+
+	figure;
+	ratio = zeros(funcLength,1);
+	for i=1:dataLength
+		dataset=strtrim(dataNames(i,:));
+		subplot(vcount,hcount,i);
+		A = ALL(:,:,i);
+		SortAndBarh(A(:,5),dataset,funcNames,0:0.2:1);
+		ratio = ratio+A(:,5);
+	end
+	ratio = ratio/dataLength;
+	figure;
+	SortAndBarh(ratio,'平均精度',funcNames,0:0.2:1);
 end
-y=1:5;
-vcount = ceil(sqrt(dataLength));
-hcount = ceil(dataLength/vcount);
-%{
-figure;
-for i=1:dataLength
-	dataset=strtrim(dataNames(i,:));
-	subplot(vcount,hcount,i);
-	A = ALL(:,:,i);
-	b=bar(y,A','grouped');title(dataset);
-	ax = b.Parent;
-	ax.XTickLabel = {'tp','fp','fn','tn','P'};
-	ax.YTick=0:0.2:1;
-end
-legend(funcNames);
-save('ALL.mat','ALL');
-%}
-figure;
-for i=1:dataLength
-	dataset=strtrim(dataNames(i,:));
-	subplot(vcount,hcount,i);
-	A = ALL(:,:,i);
-	[TP,Index] = sort( A(:,1));
-	b=barh(TP);title(dataset);
-	ax = b.Parent;
-	ax.YTickLabel = funcNames(Index,:);
-	ax.XTick=0:0.2:1;
+function SortAndBarh( InputData,Title,YTickLable,XTick )
+	[Sorted,Index] = sort( InputData );
+	b=barh(Sorted);title(Title);
+	ax=b.Parent;
+	ax.YTickLabel = YTickLable(Index,:);
+	ax.XTick=XTick;
 end
 
-figure;
-for i=1:dataLength
-	dataset=strtrim(dataNames(i,:));
-	subplot(vcount,hcount,i);
-	A = ALL(:,:,i);
-	[TP,Index] = sort( A(:,4));
-	b=barh(TP);title(dataset);
-	ax = b.Parent;
-	ax.YTickLabel = funcNames(Index,:);
-	ax.XTick=0:0.2:1;
-end
-
-figure;
-for i=1:dataLength
-	dataset=strtrim(dataNames(i,:));
-	subplot(vcount,hcount,i);
-	A = ALL(:,:,i);
-	[TP,Index] = sort( A(:,5));
-	b=barh(TP);title(dataset);
-	ax = b.Parent;
-	ax.YTickLabel = funcNames(Index,:);
-	ax.XTick=0:0.2:1;
-end
